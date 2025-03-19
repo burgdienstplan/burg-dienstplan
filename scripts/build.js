95a622b7153bddc57874ec9773ffe2aba2095bab
Martin Steindorfer <steindorfer@bv.local>
2025-03-19 11:47:58 +0100

    Füge fehlende Template-Variablen zum Build-Skript hinzu

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,29 @@
 const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
+const packageJson = require('../package.json');
 
 // Erstelle public Verzeichnis
 if (!fs.existsSync('public')) {
     fs.mkdirSync('public');
 }
 
+// Template Variablen
+const templateVars = {
+    title: 'Burg Hochosterwitz Dienstplan',
+    message: '',
+    error: '',
+    user: null,
+    Version: packageJson.version,
+    Entwickler: 'Martin Steindorfer'
+};
+
 // Liste der EJS-Dateien zum Konvertieren
 const files = ['index.ejs', 'login.ejs', 'error.ejs'];
 
 files.forEach(file => {
     const template = fs.readFileSync(path.join('views', file), 'utf-8');
-    const html = ejs.render(template, {
-        title: 'Burg Hochosterwitz Dienstplan',
-        message: '',
-        error: '',
-        user: null
-    });
+    const html = ejs.render(template, templateVars);
     
     const outputFile = file.replace('.ejs', '.html');
     fs.writeFileSync(path.join('public', outputFile), html);
